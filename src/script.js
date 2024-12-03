bfd5968951ffd1be00cd5a0b79d3bda10f0f0eac
Mariah <alinemariah@hotmail.com>
2024-12-03 20:30:59 +0100

    again

--- src/script.js
+++ src/script.js
@@ -1,13 +1,29 @@
 
-const startButton = document.getElementById('start-button');
-const background = document.getElementById('background-game');
-//const spiderMan = document.getElementById('');
-
+const startButton = document.getElementById("start-button");
 startButton.addEventListener('click', () => {
     startButton.style.display = "none"
 
-    background.style.display = "block"
+
+
+   
+
+
+
+
+    //background.style.display = "block"
     //spiderMan.style.display = "block"
 
     console.log("Start game!");
-});
+//});
+
+//const background = document.getElementById("background-game");
+
+//const restartButton = document.getElementById ("restart-game");
+//restartButton.addEventListener('click', restartGame);
+
+//function restartGame(){
+    //console.log("The player was inicialized");
+//}
+
+//const spiderMan = document.getElementById("");
+
