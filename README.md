38252551f52f5b92020a58da49385b5fe392db96
Mariah <alinemariah@hotmail.com>
2024-12-08 21:20:27 +0100

    actualization

---
+++ README.md
@@ -0,0 +1,95 @@
+
+# Spider-Man Game
+
+This project was developed exclusively for **educational purposes**. It has no commercial or profit-driven intentions, and all elements used (such as images, sounds, or references to characters) are the property of their respective owners.
+
+Welcome to the Spider-Man Game, a dynamic and interactive experience where you control Spider-Man to face challenges, dodge obstacles, and accumulate points by shooting webs and avoiding collisions. Built using HTML, CSS, and JavaScript, this game tests your reflexes and strategy.
+
+
+## Game Features
+
+### Start Screen
+
+- Enter your name to personalize the game.
+- View detailed game instructions to understand the controls and objectives.
+- Buttons for "Start" and "Instructions" provide a smooth entry into the gameplay.
+
+### Gameplay
+
+Spider-Man Controls:
+
+- Move Spider-Man in all directions using arrow keys.
+- Jump, shoot webs, and interact with the environment using keyboard controls.
+
+Dynamic Environment:
+
+- Avoid falling meteors and bouncing green balls.
+- Score points by hitting bats and spiders with webs and dodging obstacles.
+
+Physics-Driven Movements:
+
+- Gravity affects Spider-Man's jumps and falls.
+- Objects like green balls and bats move dynamically, interacting with the environment.
+
+
+### End Screen
+
+- Displays when the game ends due to collision or score loss.
+- Option to restart the game with a click of a button.
+
+
+## How to Play
+
+### Objective
+
+- Accumulate points by shooting webs at enemies and avoiding collisions.
+- Maintain your score and avoid falling below -1000 points.
+
+
+### Controls
+
+- Movement:
+    - Left: Move left.
+    - Right: Move right.
+    - Up / Spacebar: Jump.
+    - Down / Enter: Shoot webs.
+
+- Pause/Resume:
+    - Press P to pause or resume the game.
+
+- Special Features:
+    - PageUp/PageDown: Adjust Spider-Man's Z-axis scale (size).
+
+
+### Tips
+
+- Avoid meteors and bouncing balls to stay in the game.
+- Use webs strategically to eliminate enemies and earn points.
+- Keep an eye on Spider-Man's invincibility status after being hit.
+
+
+## How to Run the Game
+
+Go to https://mah30.github.io/marvel-game-project/ to play the game.
+
+
+## Requirements 
+
+- A modern browser (Google Chrome, Firefox, Edge, etc.).
+- JavaScript must be enabled in the browser.
+- A minimum screen resolution of 1024x768 is recommended.
+
+
+### Restarting the Game:
+
+- Click "Restart" on the End Screen to play again.
+
+
+### License
+
+This project is licensed under the MIT License.
+
+
+### Acknowledgements
+
+Thank you for playing Spider-Man Game! We hope you enjoy the challenge.
--- src/script.js
+++ src/script.js
@@ -1,165 +1,165 @@
-// Meteor
+// Start page
 
-let meteorX = -1000, meteorY = 99999;
-
-const screenHeight = 1000;
-const timerInterval = 40; //ms
-const meteorSpeed = screenHeight * timerInterval / 5000; // Regra de três
-
-const spiderMan = {
-    x: 0,
-    y: 0,
-    z: 0,
-};
-
-const webs = [];
-
-
-
-// Função para criar e disparar teias.
-function shootWebs() {
-    const web = document.createElement("div"); // Cria um novo elemento `div`.
-    web.classList.add("web"); // Adiciona a classe `web` ao elemento.
-    web.innerText = "WEB"; // Define o texto "WEB" no elemento.
-    web.style.left = `${spiderMan.x + 220}px`; // Define a posição X inicial da teia.
-    web.style.bottom = `${spiderMan.y + 260}px`; // Define a posição Y inicial da teia.
-    document.body.append(web); // Adiciona a teia ao corpo do documento.
-    webs.push(web); // Armazena a teia no array `webs`.
+const nameInput = document.getElementById("your-name");
+/** Only enable the start button, if a name has been typed by the player. */
+function updateFormState() {
+    const name = nameInput.value.trim();
+    document.getElementById("start").disabled = (name === "");
 }
+nameInput.addEventListener("input", updateFormState)
 
+updateFormState();
+
+document.getElementById("start").addEventListener("click", () => {
+    document.getElementById("startScreen").style.display = "none";
+    document.getElementById("game-container").style.display = "flex";
+
+    runGame();
+})
+
+document.getElementById("instructions").addEventListener("click", () => {
+    document.getElementById("help").classList.toggle("hidden");
+})
+
+
+// End screen
+
+const endScreen = document.getElementById("endScreen")
 const restartButton = document.getElementById("restart")
-document.addEventListener('click', () => {
+// Reload the game to start over
+restartButton.addEventListener('click', () => {
     location.reload()
 })
 
-// Adiciona evento para capturar teclas pressionadas.
-document.addEventListener("keydown", (event) => {
-    switch (event.key) {
-        case "ArrowRight":
-            spiderMan.x += 10; // Move para a direita.
-            break;
-        case "ArrowLeft":
-            spiderMan.x -= 10; // Move para a esquerda.
-            break;
-        case "ArrowUp":
-            spiderMan.z += 1; // Modifica a escala no eixo Z 
-            break;
-        case "ArrowDown":
-            spiderMan.z -= 1; // Modifica a escala no eixo Z (não essencial para movimento).
-            break;
-        case " ": // Espaço para "pular".
-            spiderMan.y += 250; // Aumenta a posição Y (salto).
-            break;
-        case "Enter": // Tecla Enter para disparar teias.
-            shootWebs();
-            break;
-    }
-});
-const gameTimer = window.setInterval(gameLoope, timerInterval);
 
-function didCollide(obstacle){
-    const spiderRect = document.getElementById("spiderman").getBoundingClientRect()
-    const obstacleRect = obstacle.getBoundingClientRect()
+// Main game page
 
-    return (spiderRect.left < obstacleRect.right && spiderRect.right > obstacleRect.left && spiderRect.top < obstacleRect.bottom && spiderRect.bottom > obstacleRect.top)
+const timerInterval = 1000 / 60 // Executa a cada 16ms (~60 FPS).
 
+let score = 0;
+let scoreDisplay = document.getElementById("points")
+function updateScore() {
+    scoreDisplay.innerText = String(score);
 }
 
-function gameLoope() {
-    if (meteorY > screenHeight) {
-        meteorY = 0;
-        meteorX = Math.random() * gameScreenWidth;
+// Todó os elementos
+const gameScreen = document.getElementById("game-container");
+const spiderManElement = document.getElementById("spiderman"); // Seleciona o elemento do Spider-Man.
+const greenBall = document.getElementById("ball-element");
+const meteorElement = document.getElementById("rock-meteor");
+// Created when player shoots
+const webs = [];
+
+// Initially the game screen is not shown, so we cannot know it's dimensions until the game has started.
+let gameScreenWidth, gameScreenHeight;
+function initGameScreen() {
+    gameScreenWidth = gameScreen.getBoundingClientRect().width
+    gameScreenHeight = gameScreen.getBoundingClientRect().height
+}
+
+// Spider Man
+
+const groundY = 0;
+const invinciblePeriod = Math.ceil(2000 / timerInterval);
+const spiderMan = {
+    x: 0,
+    y: groundY,
+    z: 0,
+    xSpeed: 0,
+    ySpeed: 0,
+    invincible: invinciblePeriod,
+};
+const gravity = 0.3;
+function updateSpiderMan() {
+    const spiderManWidth = spiderManElement.getBoundingClientRect().width;
+    const spiderManHeight = spiderManElement.getBoundingClientRect().height;
+
+    // Lógica para "cair" após o pulo.
+    spiderMan.y = Math.max(groundY, spiderMan.y - spiderMan.ySpeed);
+    spiderMan.x += spiderMan.xSpeed;
+    if (spiderMan.y > gameScreenHeight - spiderManHeight) {
+        spiderMan.xSpeed *= 0.5;
+        spiderMan.ySpeed += 2 * gravity;
+        spiderMan.y = (spiderMan.y + gameScreenHeight - spiderManHeight) / 2;
+    } else if (spiderMan.y > groundY) {
+        spiderMan.xSpeed *= 0.9;
+        spiderMan.ySpeed += gravity;
+    } else {
+        spiderMan.y = groundY;
+        spiderMan.xSpeed *= 0.8;
+        spiderMan.ySpeed = 0;
     }
-    if ( didCollide (meteorElement)){
 
-        const endScreen = document.getElementById("endScreen")
-        gameScreen.style.display = "none"
-        endScreen.style.display = "block"
+    if (spiderMan.x + spiderManWidth > gameScreenWidth) {
+        spiderMan.xSpeed = -1;
+        spiderMan.x = gameScreenWidth - spiderManWidth;
+    } else if (spiderMan.x < 0) {
+        spiderMan.xSpeed = 1;
+        spiderMan.x = 0;
     }
 
-     if ( didCollide (greenBall)){
-
-        const endScreen = document.getElementById("endScreen")
-        gameScreen.style.display = "none"
-        endScreen.style.display = "block"
-    } 
-
-    meteorElement.style.left = `${meteorX}px`;
-    meteorElement.style.top = `${meteorY}px`;
-
-    const spiderManElement = document.getElementById("spiderman"); // Seleciona o elemento do Spider-Man.
-    spiderManElement.style.width = "200px"
-    spiderManElement.style.height = "200px"
+    spiderMan.invincible = Math.max(0, spiderMan.invincible - 1);
 
     // Atualiza a posição do Spider-Man no DOM.
     spiderManElement.style.left = `${spiderMan.x}px`; // Atualiza a posição X.
     spiderManElement.style.bottom = `${spiderMan.y}px`; // Atualiza a posição Y.
-    spiderManElement.style.transform = `scale(1-1/${spiderMan.z})`; // Aplica transformação (escala).
-
-    // Força a posição de rolagem (se necessário).
-    window.scroll(0, 0);
-
-    // Lógica para "cair" após o pulo.
-    if (spiderMan.y >= 100) {
-        spiderMan.y = Math.max(100, spiderMan.y - 5); // Retorna ao solo gradualmente.
-    }
-
-    // Atualiza a posição das teias disparadas.
-    for (const web of webs) {
-        const posX = Number(web.style.left.replace("px", "")); // Pega a posição X da teia.
-        const posY = Number(web.style.bottom.replace("px", "")); // Pega a posição Y da teia.
-        web.style.left = `${posX + 10}px`; // Move a teia para frente no eixo X.
-        web.style.bottom = `${posY + 1}px`; // Move a teia para cima no eixo Y.
-    }
-
-    
-    // Enforce scroll position
-    window.scroll(0, 0);
-
-    meteorY += meteorSpeed;
-
+    spiderManElement.style.transform = `scale(${Math.exp(-spiderMan.z/10)})`; // Aplica transformação (escala).
+    // Flash while invincible
+    spiderManElement.style.opacity = (1 + Math.cos(spiderMan.invincible / 10)) / 2
+}
+// Reset and be temporarily invincible
+function respawnSpiderMan() {
+    spiderMan.x = 0;
+    spiderMan.y = groundY;
+    spiderMan.invincible = invinciblePeriod;
 }
 
+// Meteor
 
+let meteorX = -1000, meteorY = 99999, meteorSpeed;
+function initMeteor() {
+    meteorSpeed = gameScreenHeight * timerInterval / 5000; // Regra de três
+}
+function updateMeteor() {
+    if (meteorY > gameScreenHeight) {
+        meteorY = 0;
+        meteorX = (0.1 + Math.random() * 0.8) * gameScreenWidth;
+    }
+    meteorY += meteorSpeed;
 
-// green ball position
+    meteorElement.style.left = `${meteorX}px`;
+    meteorElement.style.top = `${meteorY}px`;
+}
 
-const gameScreen = document.getElementById("game-container");
+// Green ball
 
 let greenBallXPosition = 350
 let greenBallYPosition = 50
-let greenBallXDirection = 5; 
-let greenBallYDirection = 3; 
-
-const greenBall = document.getElementById("ball-element");
-greenBall.style.width = '120px';
-greenBall.style.height = '120px';
-greenBall.style.borderRadius = '80px';
-greenBall.style.top = `${greenBallYPosition}px`;
-greenBall.style.left = `${greenBallXPosition}px`;
-
-gameScreen.appendChild(greenBall);
-
-
-
-const gameScreenWidth = gameScreen.getBoundingClientRect().width
-const gameScreenHeight = gameScreen.getBoundingClientRect().height
-const greenBallWidth = parseInt(greenBall.style.width, 10); 
-const greenBallHeight = parseInt(greenBall.style.height, 10); // Converte para número.
-
-const meteorElement = document.getElementById("rock-meteor");
-
-function intersect(elem1, elem2) {
-    const rect1 = elem1.getBoundingClientRect()
-    const rect2 = elem2.getBoundingClientRect()
-
-    return (
-        (rect1.right > rect2.left && rect1.left < rect2.right)
-        && (rect1.bottom > rect2.top && rect1.top < rect2.bottom)
-    )
+let greenBallXDirection = 5;
+let greenBallYDirection = 3;
+let greenBallHeight, greenBallWidth;
+function initGreenBall() {
+    greenBallWidth = greenBall.getBoundingClientRect().width;
+    greenBallHeight = greenBall.getBoundingClientRect().height;
+    greenBall.style.top = `${greenBallYPosition}px`;
+    greenBall.style.left = `${greenBallXPosition}px`;
 }
+function updateGreenBall() {
+    const repell = intersect(greenBall, meteorElement);
+    const hit = !spiderMan.invincible && intersect(spiderManElement, greenBall, 10);
+    if (repell === "x" || hit === "x") {
+        greenBallXDirection *= -1;
+        greenBallXPosition += 3 * greenBallXDirection; // Atualiza a posição no eixo X.
+    } else if (repell === "y" || hit === "y") {
+        greenBallYDirection *= -1;
+        greenBallYPosition += 3 * greenBallYDirection; // Atualiza a posição no eixo Y.
+    }
 
-setInterval (() => {
+    if (hit) {
+        score -= 100;
+        respawnSpiderMan();
+    }
+
     // Verifica colisão no eixo X
     if (greenBallXPosition < 0 || greenBallXPosition > gameScreenWidth - greenBallWidth) {
         greenBallXDirection *= -1; // Inverte a direção horizontal.
@@ -175,11 +175,312 @@ setInterval (() => {
 
     greenBallYPosition += 2 * greenBallYDirection; // Atualiza a posição no eixo Y.
     greenBall.style.top = `${greenBallYPosition}px`; // Aplica o estilo atualizado.
+}
 
-    if (intersect(greenBall, meteorElement)) {
-        greenBallXDirection *= -1;
+// Webs
+
+// Função para criar e disparar teias.
+function shootWebsLeft() {
+    const web = document.createElement("div");
+    web.classList.add("web");
+    web.classList.add("moveLeft");
+    web.innerText = "WEB";
+    web.style.left = `${spiderMan.x + 20}px`;
+    web.style.bottom = `${spiderMan.y + 140}px`;
+    gameScreen.append(web);
+    webs.push(web);
+}
+function shootWebsRight() {
+    const web = document.createElement("div");
+    web.classList.add("web");
+    web.classList.add("moveRight");
+    web.innerText = "WEB";
+    web.style.left = `${spiderMan.x + 140}px`;
+    web.style.bottom = `${spiderMan.y + 140}px`;
+    gameScreen.append(web);
+    webs.push(web);
+}
+function updateWebs() {
+    // Atualiza a posição das teias disparadas.
+    const toDelete = [];
+    for (const web of webs) {
+        const posX = Number(web.style.left.replace("px", "")); // Pega a posição X da teia.
+        const posY = Number(web.style.bottom.replace("px", "")); // Pega a posição Y da teia.
+        const speedX = web.classList.contains("moveRight") ? 10 : -10;
+        web.style.left = `${posX + speedX}px`; // Move a teia para frente no eixo X.
+        web.style.bottom = `${posY + 1}px`; // Move a teia para cima no eixo Y.
+
+        for (const bat of bats) {
+            if (intersect(web, bat.elem, 0)) {
+                bat.hit();
+                score += 10;
+            }
+        }
+
+        if (intersect(web, spider.elem, 0)) {
+            spider.hit();
+            score += 25;
+        }
+
+        if (0 >  posX || posX > gameScreenWidth) {
+            toDelete.push(web);
+        }
     }
-}, 1000 / 60); // Executa a cada 16ms (~60 FPS).
+
+    // Clean up our webs
+    toDelete.forEach(web => {
+        gameScreen.removeChild(web)
+        webs.splice(webs.indexOf(web), 1);
+    });
+}
 
 
+// Bats
 
+class Bat {
+    /**
+     * Instantiate a new Bat in the game. The element needs to already exist in the game-container, be absolute positioned and have a size defined.
+     * This class describes its movement along a paraboly.
+     *
+     * @param {string} id Id of the dom element
+     * @param {number} xFactor How far across the screen the bat flies. Negative for left to right.
+     * @param {number} yFactor How far down the bat flies. From 0 to 1
+     * @param {number} wait Number of iterations to wait before reappaering after having crossed the screen.
+     */
+    constructor(id, xFactor, yFactor, wait) {
+        this.elem = document.getElementById(id);
+        this.speed = 1 / 100;
+        this.xFactor = xFactor;
+        this.yFactor = yFactor;
+        this.wait = wait / 100;
+        // Wait some time before first appearence
+        this.t = -this.wait / 2;
+    }
+
+    /** Bat has been hit by a web */
+    hit() {
+        this.t = 1;
+        this.update();
+    }
+
+    /** Increment the position by one step. The bat needs 100 steps to cross the screen */
+    fly() {
+        if (this.t >= 1 + this.wait) {
+            this.t = 0;
+        } else {
+          this.t += this.speed
+        }
+    }
+
+    /** Update the dom element with the state of the bat */
+    update() {
+        this.elem.style.left = `${this.x}px`;
+        this.elem.style.top = `${this.y}px`;
+        this.elem.classList.toggle("hidden", this.t <= 0 || this.t >= 1)
+    }
+
+    /** Calculate horizontal screen position */
+    get x() {
+        return (0.5 + (0.5-this.t)*this.xFactor) * gameScreenWidth
+    }
+    /** Calculate vertical screen position */
+    get y() {
+        return -4*this.yFactor*this.t*(this.t-1) * gameScreenHeight
+    }
+}
+
+// Define two bats
+const bats = [
+    // Bat from right to left
+    new Bat("bat1", 1, 0.8, 115),
+    // Bat from left to right
+    new Bat("bat2", -1, 0.6, 171),
+]
+
+function updateBats() {
+    // Animate all bats
+    bats.forEach(bat => {
+        bat.fly()
+        bat.update()
+    })
+}
+
+// Spiders
+
+class Spider {
+    /** Instantiate a new spider in the game. The element is added to the game-container. */
+    constructor(wait) {
+        this.width = 100;
+
+        this.elem = document.createElement("img");
+        this.elem.src = "img/spider.png";
+        this.elem.style.position = "absolute";
+        this.elem.style.width = `${this.width}px`;
+        this.elem.style.height = "auto";
+        gameScreen.appendChild(this.elem);
+
+        this.speed = 1 / 160;
+        this.wait = wait / 100;
+        // Wait some time before first appearence
+        this.t = -this.wait / 2;
+
+        this.update();
+    }
+
+    init() {
+        this.x = Math.random() * (gameScreenWidth - this.width);
+    }
+
+    /** Bat has been hit by a web */
+    hit() {
+        this.t = 1;
+        this.update();
+    }
+
+    /** Increment the position by one step. The spider needs 160 steps between appearing and disappearing again */
+    crawl() {
+        if (this.t >= 1 + this.wait) {
+            this.init();
+            this.t = 0;
+        } else {
+          this.t += this.speed
+        }
+    }
+
+    /** Update the dom element with the state of the bat */
+    update() {
+        this.elem.style.left = `${this.x}px`;
+        this.elem.style.top = `${this.y}px`;
+        this.elem.classList.toggle("hidden", this.t <= 0 || this.t >= 1)
+    }
+
+    /** Calculate vertical screen position */
+    get y() {
+        return gameScreenHeight * Math.min(0.7, 1 - 2 * Math.abs(this.t - 0.5));
+    }
+}
+
+const spider = new Spider(200);
+
+/**
+ * Check if two elements intersect.
+ *
+ * If they intersect the function returns whether they intersect more in the x or in the y direction.
+ * Otherwise return false.
+ *
+ * Subtract the given allowed overlap
+ *
+ * Can be used to detect collissions.
+ */
+function intersect(elem1, elem2, allowedOverlap) {
+    const rect1 = elem1.getBoundingClientRect()
+    const rect2 = elem2.getBoundingClientRect()
+
+    const intersectX = Math.max(0, Math.min(rect1.right, rect2.right) - Math.max(rect1.left, rect2.left) - allowedOverlap);
+    const intersectY = Math.max(0, Math.min(rect1.bottom, rect2.bottom) - Math.max(rect1.top, rect2.top) - allowedOverlap);
+    if (intersectX > intersectY && intersectY > 0) {
+        return "x";
+    }
+    if (intersectY >= intersectX && intersectX > 0) {
+        return "y";
+    }
+    return false;
+}
+
+// Key handling
+
+let rightPressed = false;
+let leftPressed = false;
+let paused = false;
+let gameTimer;
+
+function onKeyDown(event) {
+    switch (event.key) {
+        case "ArrowRight":
+            rightPressed = true;
+            break;
+        case "ArrowLeft":
+            leftPressed = true;
+            break;
+        case "PageUp":
+            spiderMan.z += 1; // Modifica a escala no eixo Z
+            break;
+        case "PageDown":
+            spiderMan.z -= 1; // Modifica a escala no eixo Z (não essencial para movimento).
+            break;
+        case " ": // Espaço para "pular".
+        case "ArrowUp":
+            spiderMan.y += 50; // Aumenta a posição Y (salto).
+            spiderMan.ySpeed -= 5;
+            break;
+        case "Enter": // Tecla Enter para disparar teias.
+        case "ArrowDown":
+            shootWebsRight();
+            shootWebsLeft();
+            break;
+        case "p":
+            paused = !paused;
+            break;
+    }
+}
+
+function onKeyUp(event) {
+    switch (event.key) {
+        case "ArrowRight":
+            rightPressed = false;
+            break;
+        case "ArrowLeft":
+            leftPressed = false;
+            break;
+    }
+}
+
+/** Do actions for keys held down */
+function handleHoldKeys() {
+    if (rightPressed && !leftPressed) {
+            spiderMan.x += 10;
+            spiderMan.xSpeed += 1;
+    } else if (leftPressed && !rightPressed) {
+            spiderMan.x -= 10; // Move para a esquerda.
+            spiderMan.xSpeed -= 1;
+    }
+}
+
+function gameLoope() {
+    // Força a posição de rolagem (se necessário).
+    window.scroll(0, 0);
+
+    if (paused) {
+        return;
+    }
+
+    if (!spiderMan.invincible && intersect(spiderManElement, meteorElement, 30) || score <= -1000) {
+        gameScreen.style.display = "none"
+        endScreen.style.display = "block"
+    }
+
+    handleHoldKeys();
+
+    updateMeteor();
+    updateSpiderMan();
+    updateBats();
+    spider.crawl()
+    spider.update();
+    updateWebs();
+    updateGreenBall();
+    updateScore();
+}
+
+function runGame() {
+    // Elements is visible now
+    initGameScreen();
+    initMeteor();
+    initGreenBall();
+    spider.init();
+
+    window.setInterval(gameLoope, timerInterval);
+
+    // Adiciona evento para capturar teclas pressionadas.
+    document.addEventListener("keydown", onKeyDown);
+    document.addEventListener("keyup", onKeyUp);
+}
